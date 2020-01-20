09b36f9337335218ed457761d9e5e7a5a0dc0f38
Amir Shiati <amirshiati98@gmail.com>
2020-01-20 13:06:41 -0500

    Textviews will dynamically grow or shrink as the text changes.

--- app/src/main/java/com/amirshiati/quotes/consts/Consts.java
+++ app/src/main/java/com/amirshiati/quotes/consts/Consts.java
@@ -5,7 +5,7 @@ public class Consts {
     public static String userName = "admin";
     public static String passWord = "changeme";
 
-    public static String getAllQuotesURL = "http://10.0.3.2:8000/api/quotes/getall";
+    public static String getAllQuotesURL = "http://10.0.2.2:8000/api/quotes/getall";
 
     public static int requestTimeOutMilliSecondes = 5000;
 }
